6e072eb81cc5fb59c11ddae238126f1e5ab0a449
Abhishek U <abhishekunni1289@gmail.com>
2020-11-19 13:05:45 +0530

    java code about to be designed

---
+++ app/src/main/java/com/example/project_login/buying_page.java
@@ -0,0 +1,14 @@
+package com.example.project_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class buying_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buying_page);
+    }
+}
---
+++ app/src/main/java/com/example/project_login/search_result_page.java
@@ -0,0 +1,14 @@
+package com.example.project_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class search_result_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result_page);
+    }
+}
